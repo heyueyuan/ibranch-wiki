397bcbaae9df33a2a859bf12fbe61406eab0e0a2
Yueyuan He <heyueyuan901@gmail.com>
2021-02-27 12:34:15 -0500

    initialize

---
+++ src/main/java/cn/heyueyuan/ibranchwiki/IbranchWikiApplication.java
@@ -0,0 +1,13 @@
+package cn.heyueyuan.ibranchwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbranchWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbranchWikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/heyueyuan/ibranchwiki/IbranchWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.heyueyuan.ibranchwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbranchWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
